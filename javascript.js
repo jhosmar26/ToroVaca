7c07452631244cae3817a7261fc8bcce895a437d
Jhose Portillo <jhosmar26.00@gmail.com>
2020-06-30 21:31:27 -0500

    js handleClick

---
+++ javascript.js
@@ -0,0 +1,11 @@
+console.log("hola");
+function handleClick(){
+    var value = "1";
+    document.getElementById('mainNumber-1').innerText = value;
+}
+
+
+/* agregar evento onclick en el boton 1
+capturar el boton
+capturar el texto del boton
+reemplazar el primer mainNumber con el texto 1 */
