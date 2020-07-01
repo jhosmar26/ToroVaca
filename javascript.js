6f57a194c5669c4733d8268f10c5f9560f871d61
paulportillo <yummta@gmail.com>
2020-06-30 22:37:53 -0500

    Update main number

--- javascript.js
+++ javascript.js
@@ -1,11 +1,36 @@
-console.log("hola");
-function handleClick(){
-    var value = "1";
-    document.getElementById('mainNumber-1').innerText = value;
+// Get elements
+const $buttonNumbers = document.querySelectorAll(".js-number-button");
+const $number1 = document.querySelector("#mainNumber-1");
+const $number2 = document.querySelector("#mainNumber-2");
+const $number3 = document.querySelector("#mainNumber-3");
+const $number4 = document.querySelector("#mainNumber-4");
+
+// Functions
+const isAvailable = (valueNumber) => {
+  if ($number1.innerText === "?") {
+    $number1.innerText = valueNumber;
+    return;
+  }
+  if ($number2.innerText === "?") {
+    $number2.innerText = valueNumber;
+    return;
+  }
+  if ($number3.innerText === "?") {
+    $number3.innerText = valueNumber;
+    return;
+  }
+  if ($number4.innerText === "?") {
+    $number4.innerText = valueNumber;
+    return;
+  }
+};
+
+const updateMainNumber = (event) => {
+  const valueNumber = event.target.innerText;
+  isAvailable(valueNumber);
+};
+
+// Handle Events
+for (let index = 0; index < $buttonNumbers.length; index++) {
+  $buttonNumbers[index].addEventListener("click", updateMainNumber);
 }
-
-
-/* agregar evento onclick en el boton 1
-capturar el boton
-capturar el texto del boton
-reemplazar el primer mainNumber con el texto 1 */
